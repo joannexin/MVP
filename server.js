b80e33d0d192d7927b4dd7c1eb401a1cca8770f6
Joanne <jeffiefeifei@gmail.com>
2016-10-29 15:24:33 -0700

    changed path of server

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/modules', express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/modules', express.static(path.join(__dirname, 'node_modules')));
 
 var port = process.env.port || 3000;
 
